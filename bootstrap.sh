090fdcaa1f0b407db9b6eeb2e37165bd0d9bf041
ghost <(none)>
2026-04-06 10:39:28 +0200

    fix jack pkg issue

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 set -eu pipefail
 
 # Remove jack2 to avoid conflict with pipewire-jack
-paru -Rdd --noconfirm jack2 2>/dev/null || true
+paru -Rdd --noconfirm jack 2>/dev/null || true
 
 ./scripts/install.sh
 ./bin/mos-pkg-aws-cli
